4e02327fd2944c8cd787a86ea1ded31b2add111f
Dar <darnellsanchez2011@gmail.com>
2023-03-19 21:06:20 -0600

    Solved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,14 @@ import { LandingPage } from './pages/LandingPage'
 function App() {
 
   return (
-<<<<<<< HEAD
     <>
       <NavBar/>
       <div className="App">
-        <h1><img src="src\assets\logo 2.png"/></h1>
+        <h1><img src="assets/img/logo_2.png"/></h1>
       </div>
       <LandingPage/>
     </>
-    
-    
-=======
-    <div className="App">
-       <h1><img src="assets/img/logo_2.png"/></h1>
-    </div>
->>>>>>> 38f434bd4122b05005a64e31a02cea2482d48f93
+
   )
 }
 
